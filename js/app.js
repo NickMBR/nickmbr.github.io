0e4fa17361ddd407bd7b333a4a99163397e6481d
NickMBR <nickmbr@gmail.com>
2017-02-07 17:11:02 -0200

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+(function(angular) {
+  'use strict';
+angular.module('sourcesdk_app', ['ngRoute'])
+  .config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider
+      .when('/sourcesdk', {
+        templateUrl: 'sourcesdk.html'
+      })
+      .when('/oprojeto', {
+        templateUrl: 'oprojeto.html'
+      })
+      .when('/faq', {
+        templateUrl: 'faq.html',
+        controller: 'cont_collapseCtrl'
+      })
+      .when('/artigos', {
+        templateUrl: 'artigos.html'
+      })
+      .when('/downloads', {
+        templateUrl: 'downloads.html',
+        controller: 'cont_collapseCtrl'
+      })
+      .when('/A-Seriedade-de-ser-um-Level-Designer', {
+        templateUrl: 'artigos/A-Seriedade-de-ser-um-Level-Designer.html'
+      })
+      .otherwise({
+        templateUrl: 'sourcesdk.html',
+      });
+
+      $locationProvider.html5Mode(true);
+  }])
+})(window.angular);
+
+angular.module('sourcesdk_app').controller('cont_collapseCtrl', ['$scope', function ($scope) {
+
+   $scope.load = function() {
+       $(".cont-collapse").click(function () {
+					$header = $(this);
+					$content = $header.next();
+					$content.slideToggle(250);
+				});
+   };
+
+   $scope.load();
+}]);
